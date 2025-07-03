e47115b6015618c3259c62b5c234dba6b5fb870d
Roshan Paudel <paudelroshan93@gmail.com>
2025-07-03 18:43:08 +1000

    updated package json and post put delete method added

--- server.js
+++ server.js
@@ -3,12 +3,29 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
+
 app.get("/", (res, req) => {
-  console.log("server get");
   res.json({
     message: "get method",
   });
 });
+app.post("/", (res, req) => {
+  res.json({
+    message: "post method",
+  });
+});
+app.put("/", (res, req) => {
+  res.json({
+    message: "put method",
+  });
+});
+app.delete("/", (res, req) => {
+  res.json({
+    message: "delete method",
+  });
+});
+
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
